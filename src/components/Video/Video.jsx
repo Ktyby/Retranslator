bbfd4a6661d1fb783656f15f9fb8aa047cee51a2
Ktyby <kutskolukes@gmail.com>
2021-01-04 18:53:36 +0300

    FEATURE/VIDEO: fix error with camera

--- src/components/Video/Video.jsx
+++ src/components/Video/Video.jsx
@@ -1,19 +1,17 @@
-import React, { useRef } from 'react';
+import React from 'react';
 
 const Video = () => { 
-  const video = useRef();
-
-  if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-    navigator.mediaDevices.getUserMedia({ video: true, audio: true  }).then((stream) => {
-        video.srcObject = stream;
-        video.play();
-    });
-  } 
+  const getRefVideo = (video) => {
+    if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+      navigator.mediaDevices.getUserMedia({ video: true, audio: true  }).then((stream) => {
+          video.srcObject = stream;
+          video.play();
+      });
+    } 
+  }
 
   return (
-    <section>
-      <video ref={(camera) => { camera = video }} autoPlay playsInline width="640" height="480"></video>
-    </section>
+    <video ref={(video) => { getRefVideo(video) }} autoPlay playsInline width="640" height="480"></video>
   );
 }
 
